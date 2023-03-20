6458f8d87d9ba237a65010c5cc00c0af6162bb44
Pedro Silva <pedro.silva8769@outlook.com>
2023-03-20 20:37:22 -0300

    Updated game.py
    - added game_over method

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,8 +1,10 @@
 import pygame
 
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
+
 from dino_runner.components.dinosaur import Dinosaur
 from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
+
 
 class Game:
     def __init__(self):
@@ -15,10 +17,9 @@ class Game:
         self.game_speed = 20
         self.x_pos_bg = 0
         self.y_pos_bg = 380
-        
+
         self.player = Dinosaur()
         self.obstacle_manager = ObstacleManager()
-        
 
     def run(self):
         # Game loop: events - update - draw
@@ -27,6 +28,7 @@ class Game:
             self.events()
             self.update()
             self.draw()
+        self.game_over()
         pygame.quit()
 
     def events(self):
@@ -36,21 +38,18 @@ class Game:
 
     def update(self):
         user_input = pygame.key.get_pressed()
-        self.player.update(user_input)       
-        
+        self.player.update(user_input)
         self.obstacle_manager.update(self)
-        
 
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
-        
+
         self.player.draw(self.screen)
         self.obstacle_manager.draw(self.screen)
-        
-        #pygame.display.update()
-        pygame.display.flip()
+
+        pygame.display.update()
 
     def draw_background(self):
         image_width = BG.get_width()
@@ -60,3 +59,10 @@ class Game:
             self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
             self.x_pos_bg = 0
         self.x_pos_bg -= self.game_speed
+
+    def game_over(self):
+        fonte = pygame.font.Font(None, 72)
+        fonte_game_over = fonte.render("GAME OVER", True, (0, 0, 0))
+        self.screen.blit(fonte_game_over, ((SCREEN_WIDTH / 2 - 150), (SCREEN_HEIGHT / 2)))
+        pygame.display.update()
+        pygame.time.delay(3000)
